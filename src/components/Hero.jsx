3e0efb3ee4699ac364b5bc245558896fdd3e260f
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-10 22:16:37 +0700

    none

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { heroSectionData } from "../data/HeroData";
+import { heroSectionData } from '../data/heroData';
 import '../style/App.css';
 
 const Hero = () => {
